5ef72099099127f211819b467c1b46b0e760fa26
kohashimoto <kohashimoto@bbtower.co.jp>
2022-03-02 20:32:54 +0900

    vimrc: modes.nvimの設定を追加

--- vimrc/after/plugin/plugin.lua
+++ vimrc/after/plugin/plugin.lua
@@ -7,3 +7,16 @@ require('nvim-treesitter.configs').setup {
   }
 }
 
+
+require('modes').setup {
+  colors = {
+    copy = '#fac863',
+    delete = '#ec5f67',
+    insert = '#99c794',
+    visual = '#c594c5',
+  },
+  line_opacity = 0.5,
+  focus_only = true
+}
+
+
