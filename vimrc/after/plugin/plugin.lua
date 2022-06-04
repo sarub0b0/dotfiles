d92a9983482f27a20555fa51b3f951049a357073
kosay <ekr59uv25@gmail.com>
2022-06-04 20:15:24 +0900

    vim(treesitter): インストールするファイルタイプを指定

--- vimrc/after/plugin/plugin.lua
+++ vimrc/after/plugin/plugin.lua
@@ -1,11 +1,35 @@
 if not (vim.g.vscode)  then
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained",
+  ensure_installed = {
+    "c",
+    "cmake",
+    "cpp",
+    "css",
+    "dot",
+    "go",
+    "graphql",
+    "html",
+    "javascript",
+    "json",
+    "json5",
+    "lua",
+    "make",
+    "perl",
+    "php",
+    "python",
+    "ruby",
+    "rust",
+    "toml",
+    "typescript",
+    "vim",
+    "yaml",
+  },
   sync_install = false,
 
   highlight = {
-    enable = true
+    enable = true,
+    additional_vim_regex_highlighting = false,
   }
 }
 
