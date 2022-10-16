9aaea3650b33bc92a4b81d307ecee69fb315303f
kohashimoto <kohashimoto@bbtower.co.jp>
2022-10-16 14:00:58 +0900

    nvim: telescopeでghqとdiagnosticを扱えるよう設定

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,6 +1,8 @@
+require('telescope').load_extension('ghq')
+
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
-
-
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics)
+vim.keymap.set('n', '<leader>fp', require('telescope').extensions.ghq.list)
