38de2da2fe9d524fd558452e691f3a5f66e299d1
kohashimoto <kohashimoto@bbtower.co.jp>
2022-10-16 19:54:17 +0900

    nvim(translator): ポップアップのサイズを設定

--- nvim/plugin/translator.lua
+++ nvim/plugin/translator.lua
@@ -1,4 +1,6 @@
 vim.g.translator_target_lang = 'ja'
+vim.g.translator_window_max_width = 0.8
+vim.g.translator_window_max_height = 0.8
 -- Echo translation in the cmdline
 -- vim.keymap.set('n', '<Leader>t', '<Plug>Translate', { silent = true })
 -- vim.keymap.set('v', '<Leader>t', '<Plug>TranslateV', { silent = true })
