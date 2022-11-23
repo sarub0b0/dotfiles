e83358fa5c0cb86a8871eabeee8c7089b92ceead
kosay <ekr59uv25@gmail.com>
2022-11-23 16:24:11 +0900

    wezterm: 更新

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require("wezterm")
 
 local keys = {
-  leader = {
-    key = "s",
-    mods = "CTRL",
-    timeout_milliseconds = 1000,
-  },
+  -- leader = {
+  --   key = "s",
+  --   mods = "CTRL",
+  --   timeout_milliseconds = 1000,
+  -- },
 
   keys = {
     {
@@ -18,158 +18,97 @@ local keys = {
       mods = "",
       action = "ToggleFullScreen",
     },
-     {
-      key = "v",
-      mods = "LEADER",
-      action = wezterm.action {
-        SplitHorizontal = {
-          domain = "CurrentPaneDomain",
-        },
-      },
-    },
-    {
-      key = "s",
-      mods = "LEADER",
-      action = wezterm.action {
-        SplitVertical = {
-          domain = "CurrentPaneDomain",
-        },
-      },
-    },
-    {
-      key = "h",
-      mods = "LEADER",
-      action = wezterm.action {
-        ActivatePaneDirection = "Left",
-      },
-    },
-    {
-      key = "l",
-      mods = "LEADER",
-      action = wezterm.action {
-        ActivatePaneDirection = "Right",
-      },
-    },
-    {
-      key = "k",
-      mods = "LEADER",
-      action = wezterm.action {
-        ActivatePaneDirection = "Up",
-      },
-    },
-    {
-      key = "j",
-      mods = "LEADER",
-      action = wezterm.action {
-        ActivatePaneDirection = "Down",
-      },
-    },
+    -- {
+    --   key = "v",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     SplitHorizontal = {
+    --       domain = "CurrentPaneDomain",
+    --     },
+    --   },
+    -- },
+    -- {
+    --   key = "s",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     SplitVertical = {
+    --       domain = "CurrentPaneDomain",
+    --     },
+    --   },
+    -- },
+    -- {
+    --   key = "h",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     ActivatePaneDirection = "Left",
+    --   },
+    -- },
+    -- {
+    --   key = "l",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     ActivatePaneDirection = "Right",
+    --   },
+    -- },
+    -- {
+    --   key = "k",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     ActivatePaneDirection = "Up",
+    --   },
+    -- },
+    -- {
+    --   key = "j",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     ActivatePaneDirection = "Down",
+    --   },
+    -- },
     {
       key = "l",
       mods = "ALT",
       action = "ShowLauncher",
     },
-    {
-      key = "H",
-      mods = "LEADER",
-      action = wezterm.action {
-        AdjustPaneSize = {
-          "Left",
-          5,
-        },
-      },
-    },
-    {
-      key = "J",
-      mods = "LEADER",
-      action = wezterm.action {
-        AdjustPaneSize = {
-          "Down",
-          5,
-        },
-      },
-    },
-    {
-      key = "K",
-      mods = "LEADER",
-      action = wezterm.action {
-        AdjustPaneSize = {
-          "Up",
-          5,
-        },
-      },
-    },
-    {
-      key = "L",
-      mods = "LEADER",
-      action = wezterm.action {
-        AdjustPaneSize = {
-          "Right",
-          5,
-        },
-      },
-    },
-  },
-}
-
-local colors = {
-  foreground = "#ffffff",
-  background = "#1b2b34",
-  cursor_bg = "#c0c5ce",
-  cursor_border = "#c0c5ce",
-  cursor_fg = "#ffffff",
-  selection_bg = "#4f5b66",
-  selection_fg = "#c0c5ce",
-
-  ansi = {
-    "#1b2b34",
-    "#ec5f67",
-    "#99c794",
-    "#fac863",
-    "#6699cc",
-    "#c594c5",
-    "#62b3b2",
-    "#ffffff",
-  },
-  brights = {
-    "#65737e",
-    "#ff676f",
-    "#ade0a7",
-    "#ffcc65",
-    "#73ace6",
-    "#dea7de",
-    "#70cccb",
-    "#ffffff",
-  },
-
-  tab_bar = {
-
-    background = "#1b2b34",
-
-    active_tab = {
-      bg_color = "#c0c0c0",
-      fg_color = "#1b2b34",
-    },
-
-    inactive_tab = {
-      bg_color = "#1b2b34",
-      fg_color = "#ffffff",
-    },
-
-    inactive_tab_hover = {
-      bg_color = "#6699cc",
-      fg_color = "#ffffff",
-    },
-
-    new_tab = {
-      bg_color = "#4f5b66",
-      fg_color = "#1b2b34",
-    },
-
-    new_tab_hover = {
-      bg_color = "#6699cc",
-      fg_color = "#ffffff",
-    },
+    -- {
+    --   key = "H",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     AdjustPaneSize = {
+    --       "Left",
+    --       5,
+    --     },
+    --   },
+    -- },
+    -- {
+    --   key = "J",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     AdjustPaneSize = {
+    --       "Down",
+    --       5,
+    --     },
+    --   },
+    -- },
+    -- {
+    --   key = "K",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     AdjustPaneSize = {
+    --       "Up",
+    --       5,
+    --     },
+    --   },
+    -- },
+    -- {
+    --   key = "L",
+    --   mods = "LEADER",
+    --   action = wezterm.action {
+    --     AdjustPaneSize = {
+    --       "Right",
+    --       5,
+    --     },
+    --   },
+    -- },
   },
 }
 
@@ -194,34 +133,39 @@ local default_prog = {}
 local font = {}
 
 if wezterm.target_triple == "x86_64-apple-darwin" then
-    default_prog = {
-        "/bin/zsh",
-        "-l"
-    }
+  default_prog = {
+    "/bin/zsh",
+    "-l"
+  }
 
-    font = wezterm.font("RictyDiminished Nerd Font")
+  font = wezterm.font("MesloLGS NF")
 
 elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    default_prog = {
-        "wsl",
-        "-d",
-        "Ubuntu",
-        "--cd",
-        "~",
-    }
+  default_prog = {
+    "wsl",
+    "-d",
+    "openSUSE-Tumbleweed",
+    "--cd",
+    "~",
+  }
 
-    font = wezterm.font("HackGenNerd Console")
+  font = wezterm.font("MesloLGS NF", { weight = 'Regular' })
 end
 
 
 return {
+
   font = font,
+  freetype_load_target = "Light",
+  freetype_interpreter_version = 40,
+  -- freetype_load_flags = 'NO_HINTING',
+  -- freetype_render_target = 'HorizontalLcd',
+  cell_width = 0.9,
+
 
   use_ime = true,
 
   default_prog = default_prog,
-
-  -- color_scheme = "OceanicNext",
 
   enable_tab_bar = true,
   use_fancy_tab_bar = false,
@@ -248,8 +192,8 @@ return {
 
   leader = keys.leader,
   keys = keys.keys,
-  colors = colors,
+  -- colors = colors,
+  color_scheme = 'OceanicNext',
 
   audible_bell = 'Disabled',
 }
-
