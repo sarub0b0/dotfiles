e314894e7d103b9d878b52e2cc5c6b8d52408829
kohashimoto <kohashimoto@bbtower.co.jp>
2023-04-03 13:17:42 +0900

    nvim: yamllsのスキーマ更新

--- nvim/lua/lsp/yaml.lua
+++ nvim/lua/lsp/yaml.lua
@@ -5,16 +5,20 @@ require('lspconfig').yamlls.setup {
         enable = true,
       },
       schemas = {
-        kubernetes = {
-          "manifest*/**/*.yaml",
-          "/*.k8s.yaml"
-        },
         ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {
           "/**/.gitlab-ci.yml",
           "/**/.gitlab/*.y*ml"
         },
         ["https://raw.githubusercontent.com/loft-sh/devspace/main/devspace-schema.json"] = {
           "/**/devspace*.yaml"
+        },
+        ["https://json.schemastore.org/kustomization.json"] = {
+          "/**/kustomization.yaml"
+        },
+        kubernetes = {
+          "manifest*/**/*.yaml",
+          "/*.k8s.yaml",
+          "*.yaml"
         },
       },
     },
