59e1f97354ba4d5bf364f9f031aaed6089492c48
kosay <ekr59uv25@gmail.com>
2023-07-01 19:11:46 +0900

    nvim(git): 不要な行を削除

--- nvim/plugin/git.lua
+++ nvim/plugin/git.lua
@@ -68,36 +68,3 @@ require('gitsigns').setup({
 vim.g.fugitive_no_maps = 0
 
 vim.keymap.set('n', '<Space>g', '<Cmd>G<CR>', { noremap = true, silent = true })
--- vim.api.nvim_create_autocmd({ 'WinEnter' }, {
---   pattern = '*',
---   callback = function()
---     if vim.b.git_dir then
---       vim.g.git_dir = vim.b.git_dir
---     end
---   end,
---   group = vim.g.augroup_names.my_auto_cmds
--- })
--- let g:fugitive_no_maps = 0
---
--- autocmd MyAutoCmd WinEnter * if exists('b:git_dir') | let g:git_dir = b:git_dir | endif
---
--- autocmd MyAutoCmd BufRead,BufNewFile * if exists('g:git_dir') | let b:git_dir = g:git_dir | endif
---
-local function modify_highlight_transparent_bg_color(group_name)
-  local gui = vim.api.nvim_get_hl_by_name(group_name, true)
-  local cterm = vim.api.nvim_get_hl_by_name(group_name, false)
-  local ctermfg = cterm.foreground
-  local ctermbg = nil
-
-  gui.background = nil
-  cterm.foreground = nil
-  cterm.background = nil
-
-  local hl_value = vim.tbl_extend('error', gui, {
-    ctermfg = ctermfg,
-    ctermbg = ctermbg,
-    cterm = cterm
-  })
-
-  vim.api.nvim_set_hl(0, group_name, hl_value)
-end
