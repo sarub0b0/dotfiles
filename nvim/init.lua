351eaa6fe57381804174ac7ffea60dbf3431eb3b
kohashimoto <kohashimoto@bbtower.co.jp>
2023-07-21 11:05:23 +0900

    nvim: neo-treeを3系に変更

--- nvim/init.lua
+++ nvim/init.lua
@@ -176,7 +176,7 @@ require('packer').startup({
 
     use {
       'nvim-neo-tree/neo-tree.nvim',
-      branch = "v2.x",
+      branch = "v3.x",
       requires = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
