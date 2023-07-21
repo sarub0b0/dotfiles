86f85e59266dae9ee0a4eb41976e45c9ae095c78
kohashimoto <kohashimoto@bbtower.co.jp>
2023-07-21 11:15:20 +0900

    nvim: signアイコンを変更

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -54,7 +54,7 @@ vim.keymap.set(
   { noremap = true, silent = true }
 )
 
-local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
