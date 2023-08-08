45168d47f079dc2d115cc038f998a026bd7f1a24
kohashimoto <kohashimoto@bbtower.co.jp>
2023-08-08 16:25:15 +0900

    nvim: `~/`や`node_modules/`などファイル数が多いとnvimの起動が遅いためlocal_vimrc.luaのロジックを改良

--- nvim/plugin/local_vimrc.lua
+++ nvim/plugin/local_vimrc.lua
@@ -3,12 +3,11 @@
 -- ------------------------------------------------
 
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
-  pattern = { '*' },
-  callback = function()
-    local files = vim.fs.find('.vimrc', { type = 'file', limit = math.huge })
-    for _, file in pairs(files) do
-      vim.cmd.source(file)
-    end
-  end,
-  group = vim.g.augroup_names.my_filetypes
+	pattern = { '*' },
+	callback = function()
+		if vim.fn.filereadable('.vimrc.local') == 1 then
+			vim.cmd.source('.vimrc.local')
+		end
+	end,
+	group = vim.g.augroup_names.my_filetypes
 })
