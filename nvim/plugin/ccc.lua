7d1e9ab5ae3c50d0ec7c52e77a23208000f19f62
kohashimoto <kohashimoto@bbtower.co.jp>
2023-10-14 21:25:33 +0900

    nvim(ccc): ansi_escapeの色付けを追加。デフォルトをdisableに設定

--- nvim/plugin/ccc.lua
+++ nvim/plugin/ccc.lua
@@ -4,9 +4,44 @@ if not ok then
   return
 end
 
+local picker = require('ccc').picker
+
 require('ccc').setup({
+  pickers = {
+    picker.ansi_escape({
+      foreground = "#c0c5ce",
+      background = "#1b2b34",
+      black = "#16252b",
+      red = "#ec5f67",
+      green = "#99c794",
+      yellow = "#fac863",
+      blue = "#6699cc",
+      magenta = "#c594c5",
+      cyan = "#62b3b2",
+      white = "#ffffff",
+      bright_black = "#65737e",
+      bright_red = "#ec5f67",
+      bright_green = "#99c794",
+      bright_yellow = "#fac863",
+      bright_blue = "#6699cc",
+      bright_magenta = "#c594c5",
+      bright_cyan = "#62b3b2",
+      bright_white = "#ffffff",
+    }),
+    picker.hex,
+    picker.css_rgb,
+    picker.css_hsl,
+    picker.css_hwb,
+    picker.css_lab,
+    picker.css_lch,
+    picker.css_oklab,
+    picker.css_oklch,
+  },
   highlighter = {
-    auto_enable = true,
-    lsp = true
-  }
+    auto_enable = false,
+    lsp = true,
+    excludes = {
+      "packer",
+    }
+  },
 })
