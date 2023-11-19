03d91ca22deaf613ced7ea1f335da8eb48d714a0
kosay <ekr59uv25@gmail.com>
2023-11-20 04:20:13 +0900

    nvim: 現バージョンのts_context_commentstringの設定に対応

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,10 +1,29 @@
-for _, value in ipairs({ 'nvim-ts-autotag', 'nvim-treesitter' }) do
+for _, value in ipairs({ 'nvim-ts-autotag', 'nvim-treesitter', 'ts_context_commentstring' }) do
   local ok, _ = pcall(require, value)
   if not ok then
     print(value .. ' is not installed.')
     return
   end
 end
+
+vim.g.skip_ts_context_commentstring_module = true
+
+require('ts_context_commentstring').setup({
+  enable_autocmd = false,
+  languages = {
+    javascript = {
+      __default = '// %s',
+      jsx_element = '{/* %s */}',
+      jsx_fragment = '{/* %s */}',
+      jsx_attribute = '// %s',
+      comment = '// %s'
+    },
+    typescript = {
+      __default = '// %s',
+      __multiline = '/* %s */'
+    }
+  }
+})
 
 require('nvim-ts-autotag').setup({})
 
@@ -34,22 +53,4 @@ require('nvim-treesitter.configs').setup {
   autotag = {
     enable = true,
   },
-  context_commentstring = {
-    enable = true,
-    config = {
-      javascript = {
-        __default = '// %s',
-        jsx_element = '{/* %s */}',
-        jsx_fragment = '{/* %s */}',
-        jsx_attribute = '// %s',
-        comment = '// %s'
-      },
-      typescript = {
-        __default = '// %s',
-        __multiline = '/* %s */'
-      }
-    }
-  }
 }
-
-
