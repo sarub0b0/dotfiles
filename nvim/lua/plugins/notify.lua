41fe13d2c35184cfc49b6e5b864b907096d02da9
kosay <ekr59uv25@gmail.com>
2024-07-21 23:27:11 +0900

    nvim(notify): remove lsp progress ringbuf size

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -2,11 +2,6 @@ return {
   {
     'j-hui/fidget.nvim',
     opts = {
-      progress = {
-        lsp = {
-          progress_ringbuf_size = 1024,
-        }
-      },
       notification = {
         window = {
           winblend = 0, -- &winblend for the window
