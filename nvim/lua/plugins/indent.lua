a9ce4cf368cf981d328f91199b17a2ed9066676c
kohashimoto <kohashimoto@bbtower.co.jp>
2024-08-19 14:42:06 +0900

    nvim(indent): switch from indent-blankline to hlchunk.nvim

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,30 +1,57 @@
 return {
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   config = function(_, opts)
+  --     require("ibl").setup(opts)
+  --
+  --     local hooks = require("ibl.hooks")
+  --
+  --     hooks.register(
+  --       hooks.type.WHITESPACE,
+  --       hooks.builtin.hide_first_space_indent_level
+  --     )
+  --
+  --     hooks.register(
+  --       hooks.type.WHITESPACE,
+  --       hooks.builtin.hide_first_tab_indent_level
+  --     )
+  --   end,
+  --   opts = {
+  --     enabled = true,
+  --     indent = {
+  --       char = '│',
+  --       highlight = { 'VertSplit' }
+  --     },
+  --     scope = {
+  --       enabled = true
+  --     }
+  --   }
+  -- },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function(_, opts)
-      require("ibl").setup(opts)
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true
+        },
+        indent = {
+          enable = true,
+          style = { vim.api.nvim_get_hl(0, { name = "VertSplit" }) },
+          filter_list = {
+            function(v)
+              return v.level ~= 1
+            end
+          }
 
-      local hooks = require("ibl.hooks")
-
-      hooks.register(
-        hooks.type.WHITESPACE,
-        hooks.builtin.hide_first_space_indent_level
-      )
-
-      hooks.register(
-        hooks.type.WHITESPACE,
-        hooks.builtin.hide_first_tab_indent_level
-      )
-    end,
-    opts = {
-      enabled = true,
-      indent = {
-        char = '│',
-        highlight = { 'VertSplit' }
-      },
-      scope = {
-        enabled = true
-      }
-    }
+        },
+        blank = {
+          enable = false
+        },
+        line_num = {
+          enable = true
+        }
+      })
+    end
   }
 }
