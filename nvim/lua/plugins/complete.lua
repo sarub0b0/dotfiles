0e7d78ca54935e30d944920bfd16983478a88622
kohashimoto <kohashimoto@bbtower.co.jp>
2024-09-27 12:54:21 +0900

    nvim: add lazy loading for CopilotChat.nvim
    Added lazy loading event "VeryLazy" for CopilotChat.nvim plugin to improve
    startup performance.

--- nvim/lua/plugins/complete.lua
+++ nvim/lua/plugins/complete.lua
@@ -161,6 +161,7 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
+    event = "VeryLazy",
     dependencies = {
       "github/copilot.vim",
       "nvim-lua/plenary.nvim", -- for curl, log wrapper
