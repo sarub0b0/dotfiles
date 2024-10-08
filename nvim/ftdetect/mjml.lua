bd4909a57c8112e2b89af7a8d3727b4efe837530
kohashimoto <kohashimoto@bbtower.co.jp>
2024-10-08 14:34:47 +0900

    nvim: add MJML filetype detection and eruby subtype
    - Added a new filetype detection for MJML files in Neovim.
    - Created an autocmd to set the filetype to 'eruby' for '*.mjml' files.
    - Updated eruby filetype plugin to set subtype to 'html' for MJML files.

---
+++ nvim/ftdetect/mjml.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = { '*.mjml' },
+  callback = function()
+    vim.opt_local.filetype = 'eruby'
+  end,
+  group = 'filetypedetect'
+})
