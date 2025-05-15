2d134f22adc20e940cbd1a4f104324b0096668f9
kohashimoto <kohashimoto@bbtower.co.jp>
2025-05-15 19:19:27 +0900

    nvim(telescope): add telescope-ui-select extension
    Add the telescope-ui-select.nvim plugin to enhance native Neovim
    UI selection interfaces with Telescope's UI. This provides a more
    consistent and visually appealing selection experience throughout
    the editor.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,4 +13,11 @@ return {
       }
     end,
   },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    dependencies = "nvim-telescope/telescope.nvim",
+    config = function()
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
